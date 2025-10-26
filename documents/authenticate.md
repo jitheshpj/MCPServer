ae303db3e243d752da8ba26b1211e5de1b0ea11a
jitheshpj <jitheshpj@yahoo.com>
2025-10-26 16:23:27 -0400

    documents/authenticate.md

---
+++ documents/authenticate.md
@@ -0,0 +1,2 @@
+This document helps t0 create an authentication module for MCP tool outputs.
+This file provides way for PII/PHI filtering to user output
---
+++ testsrvr.py
@@ -0,0 +1,7 @@
+def greet(name: str) -> str:
+    print ( f"Hello, {name} ! Welcome MCP")
+    return f"Hello, {name} ! Welcome MCP"
+    
+
+if __name__ == "__main__":
+    greet ("jithesh")
